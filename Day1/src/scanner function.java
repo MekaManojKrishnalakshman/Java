6922a68e48ed34133cd0397f4d4a1ce30f147fe4
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-06-19 11:49:57 +0530

    scanner function

--- Day1/src/scanner function.java
+++ Day1/src/scanner function.java
@@ -1,9 +1,11 @@
 public class scanner function {
     public static void main(String[] args){
         Scanner input= new Scanner(System.in);
-        System.out.println(x:"Enter the Name:");
-        String s =sc.next();
+        System.out.println("Enter the Name:");
+        String s =input.next();
         System.out.println("Your Name is:");
+        input.close();
+
 
 
     }
