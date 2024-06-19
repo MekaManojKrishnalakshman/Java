b391d996f4f696e130671a2e40d7dbe6124a0580
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-06-19 11:24:57 +0530

    Datatypes

---
+++ Day1/src/Datatypes.java
@@ -0,0 +1,16 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int age=22;
+        byte roll=15;
+        short admissionno=27321;
+        float attendance=78.99f;
+        double percentage=85.32;
+        char Gender='M';
+        String s="Manoj";
+        boolean student=true;
+        System.out.println(age);
+        System.out.println(roll);
+
+    }
+    
+}
