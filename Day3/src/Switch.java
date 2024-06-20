fc3b4e3f6416a66dbdd6148d8345c624b1ab8b96
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-06-20 11:54:16 +0530

    switchstatement

---
+++ Day3/src/Switch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Switch {
+    public  static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String dayname;
+        int day =sc.nextInt();
+        switch (day) {
+            case 1:
+                dayname="Monday";
+                break;
+            case 2:
+                 dayname="Tuesday";
+                 break;
+            case 3:
+                 dayname="Wednesday";
+                 break;     
+
+        
+            default:
+                dayname="Invalid Day";
+                break;
+        }
+        
+    }
+    
+}
